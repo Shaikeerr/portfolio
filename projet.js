05e493ac48fa5ff44f4f38183284dcfb0122248c
Noah Calmette <shaikeerr@gmail.com>
2023-12-13 02:44:09 +0100

    Updated project style

--- projet.js
+++ projet.js
@@ -8,16 +8,23 @@ console.log(url)
             let data = json;
             data = data[url]
 
+
             document.querySelector('.image_projet').innerHTML += "<img src=" + data["image"] +" alt='image projet'><br>"
+            document.querySelector('.liens_projet').innerHTML += "<a href=" + data["liens"]["site"] +"> <img class='logo_lien' src='logos/website.png'><p>Site Web</p> </a><br>"
+            if (data["liens"]["github"] != null) {
+                document.querySelector('.liens_projet').innerHTML += "<a href=" + data["liens"]["github"] +"> <img class='logo_lien' src='logos/github.png'><p>Github</p> </a><br>"
+            }
             document.querySelector('body .container .texte').innerHTML += "<h1>" + data["titre"] + "</h1><br>"
             document.querySelector('body .container .texte').innerHTML += "<p>" + data["description"] + "</p><br>"
-            document.querySelector('body .container .texte').innerHTML += "<a href=" + data["lien"] + ">" + data["lien"] + "</a><br>"
+            document.querySelector('body .container .texte').innerHTML += "<a href=" + data["lien"] + ">" + data["liens"]["site"] + "</a><br>"
             for (const i in data["technos"]) {
                     const techno = data["technos"][i];
                     const lien_techno = data["links"][i];
-                    console.log(lien_techno)
                     document.querySelector('.technos').innerHTML += "<a href=" + lien_techno + "><img src=" + techno + " alt='image techno'></a><br>"
+                    
             }
+            document.querySelector('.showcase').innerHTML += "<img src=" + data["showcase"]["image"] +" alt='showcase'><br>"
+            document.querySelector('.showcase').innerHTML += "<p>" + data["showcase"]["legende"] +"</p><br>"
             
             
         });
